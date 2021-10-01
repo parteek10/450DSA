d949724552b0ff07186a3a54f0f8b33a51ac3ec9
Shubhra Agarwal <67220475+shubhraagarwal@users.noreply.github.com>
2021-10-01 11:42:50 +0530

    Update kadaneAlgo.cpp

--- Array/kadaneAlgo.cpp
+++ Array/kadaneAlgo.cpp
@@ -1,17 +1,48 @@
-int maxSubarraySum(int arr[], int n)
-{
+// Link to the question : https://practice.geeksforgeeks.org/problems/kadanes-algorithm-1587115620/1
 
-    // Your code here
-    int currSum = 0;
-    int maxSum = 0;
-    for (int i = 0; i < n; i++)
-    {
-        currSum += arr[i];
-        if (currSum < 0)
-        {
-            currSum = 0;
-        }
-        maxSum = max(currSum, maxSum);
+#include<bits/stdc++.h>
+#include<iostream>
+
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(int arr[], int n){
+        
+        // Your code here
+        int currSum = 0;
+        int maxSum = 0;
+        for(int i = 0; i < n ;i++){
+            currSum += arr[i];
+            if(currSum < 0){
+                currSum = 0;
+            }
+            maxSum = max(currSum, maxSum);
+            }
+            return maxSum;
+        
     }
-    return maxSum;
+};
+
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+            
+        Solution ob;
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
 }
