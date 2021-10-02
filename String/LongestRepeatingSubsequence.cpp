05e6baa25edd0d8dacd3249cb7077577bdba9415
Shashwat-Sahu <63558563+Shashwat-Sahu@users.noreply.github.com>
2021-10-02 18:55:27 +0530

    Create LongestRepeatingSubsequence.cpp

---
+++ String/LongestRepeatingSubsequence.cpp
@@ -0,0 +1,38 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+	public:
+		int LongestRepeatingSubsequence(string str){
+		    // Code here
+		    int n= str.size();
+		    vector<vector<int> > dp(n+1,vector<int> (n+1,0));
+		    for(int i=1;i<=n;i++)
+		    {
+		        for(int j=1;j<=n;j++)
+		        {
+		            if(str[i-1]==str[j-1]&&i!=j)
+		            dp[i][j]=1+dp[i-1][j-1];
+		            else
+		            dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+		        }
+		    }
+		    return dp[n][n];
+		}
+
+};
+
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string str;
+		cin >> str;
+		Solution obj;
+		int ans = obj.LongestRepeatingSubsequence(str);
+		cout << ans << "\n";
+	}
+	return 0;
+}  
