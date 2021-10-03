263ea7aebe3c0692c1d2ff9c3c8a242b7ac5a514
Akshat Kumar <55556128+Axhat@users.noreply.github.com>
2021-10-03 09:48:03 +0530

    Added  9_commonElementsInAllRowsOfMatrix

---
+++ Matrix/9_commonElementsInAllRowsOfMatrix.cpp
@@ -0,0 +1,30 @@
+//https://www.geeksforgeeks.org/common-elements-in-all-rows-of-a-given-matrix/
+
+void printCommonElements(int mat[M][N])
+{
+    unordered_map<int, int> mp;
+ 
+    // initialize 1st row elements with value 1
+    for (int j = 0; j < N; j++)
+        mp[mat[0][j]] = 1;
+ 
+    // traverse the matrix
+    for (int i = 1; i < M; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            // If element is present in the map and
+            // is not duplicated in current row.
+            if (mp[mat[i][j]] == i)
+            {
+               // we increment count of the element
+               // in map by 1
+                mp[mat[i][j]] = i + 1;
+ 
+                // If this is last row
+                if (i==M-1 && mp[mat[i][j]]==M)
+                  cout << mat[i][j] << " ";
+            }
+        }
+    }
+}
